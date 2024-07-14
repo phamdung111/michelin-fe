e08e13367bb336a43964d3a313164fa37d0a9799
phamdung111 <dungvanx8@gmail.com>
2024-07-15 05:04:19 +0700

    Update error something

--- composables/admin/restaurant/permission/admin-permission-restaurants.composable.ts
+++ composables/admin/restaurant/permission/admin-permission-restaurants.composable.ts
@@ -3,5 +3,9 @@ import { useUiStore } from '~/store/ui';
 export const adminPermissionRestaurantComposable = async (payload: Record<number, string>) => {
     const ui = useUiStore();
     const response = await adminRestaurantService.permissionRestaurants(payload);
+    if (response.errors) {
+        ui.openNotification({ status: 'errors', message: 'somethings went wrong!' });
+        return;
+    }
     return response;
 };
